8911551080aeda2d29c592596b38365b1f05ad01
Gabrieldelsaux <gabriel.delsaux3@gmail.com>
2026-04-29 10:56:25 +0200

    test final

--- server.js
+++ server.js
@@ -8,10 +8,12 @@ const bcrypt = require('bcrypt');
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
+
 connection.connect((err) => {
   if (err) {
     console.error('Erreur de connexion à la base de données :', err);
